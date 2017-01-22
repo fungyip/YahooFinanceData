c0eb2a98b3aa36c6e16de03893563b1fbd9ecc09
fungyip <fung.yip@gmail.com>
2017-01-22 14:51:22 +0800

    New R Combined Stocks Script

---
+++ R/CombinedStocks.R
@@ -0,0 +1,133 @@
+# install.packages("tidyverse")
+library(tidyverse)
+
+# optional: set the minimum amount of time that a stock has to have historical
+# data for inclusion in the model (the entire model is limited in the data it
+# can use by youngest stock)
+time_screen = 364
+
+#get present date
+today = Sys.Date()
+today_year = format(today, "%Y") # with month as a word
+today_month = format(today, "%m") #months are one less for yahoo
+today_day = format(today, "%d")
+
+#set start date for historical look
+start_year = "2010"
+start_month = "1"
+start_day = "1"
+
+##Stock Selection
+
+#keep in mind that the model is limited by the *youngest* stock
+# if the youngest stock is one month the model only uses the historical
+# prices for all stocks for one month
+
+#恒生指數
+tickers = c(
+  "0001.HK", #長和
+  "0002.HK", #中電控股
+  "0003.HK", #香港中華煤氣
+  "0004.HK", #九龍倉集團
+  "0005.HK", #匯豐控股
+  "0006.HK",#電能實業
+  "0011.HK",#恆生銀行
+  "0012.HK",#恆基地產
+  "0016.HK",#新鴻基地產
+  "0017.HK",#新世界發展
+  "0019.HK",#太古股份公司Ａ
+  "0023.HK",#東亞銀行
+  "0027.HK",#銀河娛樂
+  "0066.HK",#港鐵公司
+  "0083.HK",#信和置業
+  "0101.HK",#恆隆地產
+  "0135.HK",#崑崙能源
+  "0144.HK",#招商局港口
+  "0151.HK",#中國旺旺
+  "0267.HK",#中信股份
+  "0293.HK",#國泰航空
+  "0322.HK",#康師傅控股
+  "0386.HK",#中國石油化工股份
+  "0388.HK",#香港交易所
+  "0494.HK",#利豐
+  "0688.HK",#中國海外發展
+  "0700.HK",#騰訊控股
+  "0762.HK",#中國聯通
+  "0823.HK",#領展房產基金
+  "0836.HK",#華潤電力
+  "0857.HK",#中國石油股份
+  "0883.HK",#中國海洋石油
+  "0939.HK",#建設銀行
+  "0941.HK",#中國移動
+  "0992.HK",#聯想集團
+  "1038.HK",#長江基建集團
+  "1044.HK",#恆安國際
+  "1088.HK",#中國神華
+  "1109.HK",#華潤置地
+  "1113.HK",#長實地產
+  "1299.HK",#友邦保險
+  "1398.HK",#工商銀行
+  "1880.HK",#百麗國際
+  "1928.HK",#金沙中國有限公司
+  "3328.HK",#交通銀行
+  "3988.HK")#中國銀行
+
+##Stock Data Extraction
+count = 0
+for(ticker in tickers)
+{
+  #read the ticker historical charts  
+  retrieved_csv <- read.csv(paste0("http://ichart.finance.yahoo.com/table.csv?s=",ticker,"&d=",toString(as.double(today_month)-1),"&e=",today_day,"&f=",today_year,"&g=d","&a=",toString(as.double(start_month)-1),"&b=",start_day,"&c=",start_year,"&ignore=.csv", collapse = ", "), sep=",", header=1 )
+  
+  write_csv(retrieved_csv, paste("./DataOut/HangSeng/", ticker,".csv", sep=""))
+  
+  #this is an awkward way to only use tickers if they have enough rows of data
+  if (nrow(retrieved_csv) > time_screen)
+  {
+    
+    #keep this ticker in the list for the model
+    if(count==0){
+      tickers2 <- c(ticker)
+    } else {
+      tickers2 <- c(tickers2,ticker)
+    }
+    # problem is we need to merge on the date, and R thinks our
+    # date fields are factors, not 'dates'
+    retrieved_csv$Date = as.POSIXct(retrieved_csv$Date, format="%Y-%m-%d")
+    
+    
+    #calculate the day change (%)
+    retrieved_csv$CloseChange <- (retrieved_csv$Close-retrieved_csv$Open)/(retrieved_csv$Open)*100
+    #filecsv$CloseChange <- (filecsv[,paste("Close_",ticker,sep="")]-filecsv[,paste("Open_",ticker,sep="")])/(filecsv[,paste("Open_",ticker,sep="")])*100
+    retrieved_csv$CloseChange <- round(retrieved_csv$CloseChange, 1)
+    
+    
+    # Optionally only keep those columns used for prediction
+    retrieved_csv_colnames <- colnames(retrieved_csv)
+    retrieved_csv_colnames = retrieved_csv_colnames[! retrieved_csv_colnames %in% "Open"]
+    retrieved_csv_colnames = retrieved_csv_colnames[! retrieved_csv_colnames %in% "Close"]
+    retrieved_csv_colnames = retrieved_csv_colnames[! retrieved_csv_colnames %in% "High"]
+    retrieved_csv_colnames = retrieved_csv_colnames[! retrieved_csv_colnames %in% "Low"]
+    retrieved_csv_colnames = retrieved_csv_colnames[! retrieved_csv_colnames %in% "Adj.Close"]
+    
+    
+    
+    retrieved_csv <- retrieved_csv[,retrieved_csv_colnames]
+    
+    #amend the column names - add ticker name suffix
+    colnames(retrieved_csv) <- paste(colnames(retrieved_csv), ticker, sep = "_")
+    #merge into one big table
+    if (count==0){
+      bigtable <- retrieved_csv
+      colnames(bigtable)[1] <- "Date" #rename this to help standardize
+    }else{
+      #merge is based on the FIRST column which is posixct DATE
+      bigtable <-merge(bigtable,retrieved_csv,by.x = paste(colnames(bigtable)[1]), by.y = paste(colnames(retrieved_csv)[1]), all=TRUE)
+      
+    }
+    
+    count = count + 1
+  }
+}
+write_csv(bigtable, "./DataOut/HangSeng/CombinedStocks.csv")
+
